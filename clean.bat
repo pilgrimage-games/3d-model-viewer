46d58855a4e0ace81009295b4830c06245c6f97f
Shawn Hatori <shawn.hatori@gmail.com>
2025-01-09 02:13:24 -0500

    clean up batch scripts

--- clean.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-pushd build\
-attrib +r *.exe
-attrib +r *.pga
-del /q * > nul 2> nul
-attrib -r *.exe
-attrib -r *.pga
-popd
---
+++ vars.bat
@@ -0,0 +1,5 @@
+@echo off
+
+set project_name=3d_model_viewer
+set project_dir=%~dp0
+set common_lib_path=!project_dir!\..\game-development\common-library
